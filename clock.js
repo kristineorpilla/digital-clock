14b77541275905703f4a19c1bc5378788ed0f55d
Your Name <you@example.com>
2018-11-29 10:50:29 -1000

    js update

--- clock.js
+++ clock.js
@@ -2,6 +2,16 @@ var d = new Date();
 var h = d.getHours();
 var m = d.getMinutes();
 
+function add0(num) {
+	if (num < 10) {
+		return num= "0" + num;
+	}
+	else {
+		return num;
+	}
+}
+
+
 function time() {
   document.getElementById("clock").innerText = h + " : " + m;
 }
