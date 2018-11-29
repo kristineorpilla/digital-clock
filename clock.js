53f0d7a7d388f5a3a2ad89d42be890ced8c88b7b
MarkIsNotTaken <mdemesillo@gmail.com>
2018-11-29 09:24:21 -1000

    other work

--- clock.js
+++ clock.js
@@ -1,24 +1,11 @@
 var d = new Date();
 var h = d.getHours();
-
-console.log(d.getHours());
-
 var m = d.getMinutes();
 
-console.log(d.getMinutes());
-
-function time(hours, minutes) {
-  return hours + " : " + minutes;
+function time() {
+  document.getElementById("clock").innerText = h + " : " + m;
 }
 
-console.log(time(h, m));
+setInterval(time, 0);
 
-function add0(minutes) {
-  if (minutes < 10) {
-    return str.
-  } 
-  else {
-
-  }
-}
 
