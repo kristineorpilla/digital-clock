3ad2d81bcb34d815f82c5b28530be13df7e84b4d
Your Name <you@example.com>
2018-11-28 15:27:57 -1000

    works

--- clock.js
+++ clock.js
@@ -1,24 +1,13 @@
 var d = new Date();
 var h = d.getHours();
-
-console.log(d.getHours());
-
 var m = d.getMinutes();
 
-console.log(d.getMinutes());
-
-function time(hours, minutes) {
-  return hours + " : " + minutes;
+function time () {
+	return h + " : " + m;
 }
 
-console.log(time(h, m));
+var clock = time();
 
-function add0(minutes) {
-  if (minutes < 10) {
-    return str.
-  } 
-  else {
+document.getElementById("clock").innerText= clock; 
 
-  }
-}
 
