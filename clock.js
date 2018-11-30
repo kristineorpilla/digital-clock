b1a80a2ed95e325a6663f8f251d50eee14d0100d
MarkIsNotTaken <mdemesillo@gmail.com>
2018-11-29 14:17:22 -1000

    finish?

--- clock.js
+++ clock.js
@@ -1,20 +1,11 @@
-var d = new Date();
-var h = d.getHours();
-var m = d.getMinutes();
-
-function add0(num) {
-	if (num < 10) {
-		return num= "0" + num;
-	}
-	else {
-		return num;
-	}
-}
-
-
 function time() {
+  var d = new Date();
+  var h = d.getHours();
+  var m = d.getMinutes();
+    if (m < 10){ 
+     m = "0" + m;
+  }
   document.getElementById("clock").innerText = h + " : " + m;
 }
 
 setInterval(time, 0);
-
