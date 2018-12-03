46846fc7b313096a4cab8eddf97e0386668a57d0
Your Name <you@example.com>
2018-12-02 20:35:07 -1000

    12hr format with am/pm; problem with midnight

--- clock.js
+++ clock.js
@@ -1,20 +1,53 @@
 function time() {
-  var d = new Date();
+  var d = new Date(2018, 11, 24, 0, 59, 30, 0);
   var h = d.getHours();
   var m = d.getMinutes();
-  if (m < 10){ 
-  	m = "0" + m;
-  }
-  else {
-  	m = m;
-  }
-  if (h < 10) {
-  	h = "0" + h;
-  }
-  else {
-  	h = h;
-  }
-  document.getElementById("clock").innerText = h + " : " + m;
+
+  var x = "AM"; 
+
+  h = midnight(h);
+  x = apm(h, x);
+  h = zero(change(h));
+  m = zero(m);
+
+
+  document.getElementById("clock").innerText = h + " : " + m + " " + x;
 }
 
 setInterval(time, 0);
+
+function zero(n) {
+  if (n < 10) {
+    return n = "0" + n;
+  }
+  else {
+    return n = n;
+  }
+}
+
+function change(h) {
+  if (h > 12) {
+    return h = h - 12;
+  }
+  else {
+    return h = h;
+  }
+}
+
+function apm(h, x) {
+  if (h > 11) {
+    return x = "PM";
+  }
+  else {
+    return x = "AM";
+  }
+}
+
+function midnight(h) {
+  if (h = 0) {
+    return h = "12";
+  }
+  else {
+    return h = h;
+  }
+}
